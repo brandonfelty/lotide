f7f452b0da550fa6294d212d25929d43e3a6a8a5
Brandon Felty <brcfelty@gmail.com>
2022-04-04 17:52:07 -0600

    practicing with assertion

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("⛔️⛔️⛔️ Assertion Failed: [" + actual + "] !== [" + expected + "]");
+  } else {
+    console.log("✅✅✅ Assertion Passed: [" + actual + "] === [" + expected + "]");
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 2);
