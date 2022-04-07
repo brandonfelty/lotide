a33f43c8a2710a1bce1dc4582513df5f7c34a4cb
Brandon Felty <brcfelty@gmail.com>
2022-04-07 17:43:53 -0600

    Practicing building function that use call backs

---
+++ takeUntil.js
@@ -0,0 +1,45 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length === array2.length) {
+    let isEqual = true;
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] === array2[i]) {
+
+      } else {
+        return false;
+      }
+    }
+    return true;
+  }
+}
+const assertArraysEqual = function(array1,  array2) {
+  if (eqArrays(array1, array2)) {
+    console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`)
+  } else {
+    console.log(`⛔️⛔️⛔️ Assertion Failed: [${array1}] !== [${array2}]`)
+  }
+}
+
+
+const takeUntil = function(array, callback) {
+  //.. 
+ const results = [];
+  
+  for (let element of array) {
+    if (!callback(element)) {
+      results.push(element);
+    } else {
+      break;
+    }
+  }
+  return results;
+}
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+assertArraysEqual(results1, [1, 2, 5, 7, 2]);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ",");
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
