c770115f443e3fcab9369a62a72a575a664e8df7
Brandon Felty <brcfelty@gmail.com>
2022-04-11 16:11:31 -0600

    refactoring more functions so that they use modules and added a test for assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,16 +1,5 @@
-const eqArrays = function(array1, array2) {
-  if (array1.length === array2.length) {
-    let isEqual = true;
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
+const eqArrays = require('./eqArrays');
 
-      } else {
-        return false;
-      }
-    }
-    return true;
-  }
-}
 const assertArraysEqual = function(array1,  array2) {
   if (eqArrays(array1, array2)) {
     console.log(`✅✅✅ Assertion Passed: [${array1}] === [${array2}]`)
@@ -19,5 +8,4 @@ const assertArraysEqual = function(array1,  array2) {
   }
 }
 
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
+module.exports = assertArraysEqual;
