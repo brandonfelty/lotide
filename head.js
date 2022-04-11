9159d89e405ec9bf9b28957df1d70c4439961077
Brandon Felty <brcfelty@gmail.com>
2022-04-11 15:02:44 -0600

    refactored assertEqual and head so that we can practice TDD

--- head.js
+++ head.js
@@ -1,16 +1,5 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`⛔️⛔️⛔️ Assertion Failed: [${actual}] !== [${expected}]`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  }
-};
-
 const head = function(input) {
   return input[0];
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["hello", "Lighthouse", "Labs"]), "hello");
-assertEqual(head([1]),1);
-assertEqual(head([]), undefined);
+module.exports = head;
