cd92ba1aa9063c340b2f19e8ce03e131257fedc0
Brandon Felty <brcfelty@gmail.com>
2022-04-11 15:27:08 -0600

    refactoring our tail function and created a seperate test file

--- tail.js
+++ tail.js
@@ -1,11 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual !== expected) {
-    console.log(`⛔️⛔️⛔️ Assertion Failed: [${actual}] !== [${expected}]`);
-  } else {
-    console.log(`✅✅✅ Assertion Passed: [${actual}] === [${expected}]`);
-  }
-};
-
 const tail = function(array) {
   let arrayTail = [];
   for (let i = 0; i < array.length - 1; i++) {
@@ -14,7 +6,5 @@ const tail = function(array) {
   return arrayTail;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
 
