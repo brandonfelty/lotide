2b61a2a0146e5953629ce2ccbc1d0ceb15cf7de8
Brandon Felty <brcfelty@gmail.com>
2022-07-28 13:27:20 -0600

    fixes nested arrays and adds tests

--- eqArrays.js
+++ eqArrays.js
@@ -1,10 +1,12 @@
 const eqArrays = function(array1, array2) {
   if (array1.length === array2.length) {
-    let isEqual = true;
     for (let i = 0; i < array1.length; i++) {
-      if (array1[i] === array2[i]) {
-      } else {
-        return false;
+      if(Array.isArray(array1[i]) || Array.isArray(array2[i])) {
+        const checkArray = eqArrays(array1[i], array2[i]);
+        if (!checkArray) return false;
+      }
+      if (!Array.isArray(array1[i]) && array1[i] !== array2[i]) {
+       return false;
       }
     }
     return true;
