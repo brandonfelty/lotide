52add700716edf916ebfbe61a99da59959144c18
Brandon Felty <brcfelty@gmail.com>
2022-08-17 09:20:52 -0600

    published v1.0.1

--- eqObjects.js
+++ eqObjects.js
@@ -30,6 +30,7 @@ const eqObjects = function(object1, object2) {
   if (keys1.length !== keys2.length) {
     return false;
   }
+  
   for (const key of keys1) {
     if (Array.isArray(object1[key]) || Array.isArray(object2[key])) {
       return eqArrays(object1[key], object2[key]);
